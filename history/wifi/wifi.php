6fbfdeda83818e92ea57a22af40f44909a8015d1
N0m@d-Xii <34090451+nuttapong1996@users.noreply.github.com>
2022-06-04 16:48:37 +0700

    add datatable template in wifi history

--- history/wifi/wifi.php
+++ history/wifi/wifi.php
@@ -12,41 +12,42 @@
 <body>
     <div class="container-fluid">
         <div class="row mt-5">
-            <div class="col"></div>
-            <div class="col-sm-12">
-                <div class="col-sm-5">
-                    <div class="input-group">
-                    <select class="form-control form-control-sm" name="" id="">
-                    <option value="">เดือน</option>
-                    <option value="01"> มกราคม</option>
-                    <option value="02"> กุมภาพันธ์</option>
-                    <option value="03"> มีนาคม</option>
-                    <option value="04"> เมษายน</option>
-                    <option value="05"> พฤษภาคม</option>
-                    <option value="06"> มิถุนายน</option>
-                    <option value="07"> กรกฎาคม</option>
-                    <option value="08"> สิงหาคม</option>
-                    <option value="09"> กันยายน</option>
-                    <option value="10"> ตุลาคม</option>
-                    <option value="11"> พฤศจิกายน</option>
-                    <option value="12"> ธันวาคม</option>
-                </select>
-                <?php 
-                $current_year = date("Y");
-                $earliest_year = 2021;
-                $last_year =date("Y");
+            <div class="col-sm-5">
+                <div class="input-group">
+                        <select class="form-control form-control-sm" name="" id="">
+                        <option value="">เดือน</option>
+                        <option value="01"> มกราคม</option>
+                        <option value="02"> กุมภาพันธ์</option>
+                        <option value="03"> มีนาคม</option>
+                        <option value="04"> เมษายน</option>
+                        <option value="05"> พฤษภาคม</option>
+                        <option value="06"> มิถุนายน</option>
+                        <option value="07"> กรกฎาคม</option>
+                        <option value="08"> สิงหาคม</option>
+                        <option value="09"> กันยายน</option>
+                        <option value="10"> ตุลาคม</option>
+                        <option value="11"> พฤศจิกายน</option>
+                        <option value="12"> ธันวาคม</option>
+                    </select>
+                    <?php 
+                    $current_year = date("Y");
+                    $earliest_year = 2021;
+                    $last_year =date("Y");
 
-                echo "<select class='form-control form-control-sm' name='' id=''>";
-                echo "<option value=''>ปี</option>";
-                foreach(range($last_year, $earliest_year) as $r){
-                    echo "<option value='$r'>$r</option>";
-                }
-                echo "</select>";
-                ?>
-                    </div>
+                    echo "<select class='form-control form-control-sm' name='' id=''>";
+                    echo "<option value=''>ปี</option>";
+                    foreach(range($last_year, $earliest_year) as $r){
+                        echo "<option value='$r'>$r</option>";
+                    }
+                    echo "</select>";
+                    ?>
                 </div>
-                
-                <table class="table">
+            </div>             
+        </div>
+        <div class="row mt-5">
+            <div class="col"></div>
+            <div class="col-sm-12">                 
+                <table class="table" id="myTable">
                     <thead>
                         <tr>
                             <th></th>
@@ -61,9 +62,7 @@
 </html>
 
 <script>
-    $(document).ready(function () {
-    $('#example').DataTable({
-        order: [[3, 'desc']],
-    });
-});
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
 </script>
